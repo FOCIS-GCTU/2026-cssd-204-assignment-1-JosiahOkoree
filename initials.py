6139c2aba90170db5e216ae8f0db5a10da156fba
JosiahOkoree <2425403080@live.gctu.edu.gh>
2026-06-08 06:51:58 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,41 @@
+# File: initials.py
+# Description: Prints stylized large initials JCO with surrounding periods.
+# Assignment Number: 1
+#
+# Name: Josiah Okoree
+# STUDENT ID: 2425403080
+# Email: 2425403080@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Josiah Okoree, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints the small initials and stylized large initials for JCO."""
+    print()
 
 
+    print("...JCO")
 
-main()
+
+    print()
+
+
+    print(".....JJJJJJJJJJ.........CCCCCCCCCC..........OOOOOOOOOO......")
+    print(".....JJJJJJJJJJ........CCCCCCCCCCCC........OOOOOOOOOOOO.....")
+    print("..........JJ...........CC..................OO........OO.....")
+    print("..........JJ...........CC..................OO........OO.....")
+    print("..........JJ...........CC..................OO........OO.....")
+    print("..........JJ...........CC..................OO........OO.....")
+    print("..........JJ...........CC..................OO........OO.....")
+    print("...JJ.....JJ...........CC..................OO........OO.....")
+    print("...JJJJJJJJJ......**...CCCCCCCCCCCC...**...OOOOOOOOOOOO...**")
+    print("....JJJJJJJ.......**....CCCCCCCCCC....**....OOOOOOOOOO....**")
+
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
